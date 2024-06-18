fe1e56a0da96eb9e3d4d6867391e2a697c3722f5
Matheus Ricardo <matheusri1@hotmail.com>
2024-06-18 10:31:52 -0300

    Create problem_1059.java

---
+++ Level_1/problem_1059.java
@@ -0,0 +1,8 @@
+public class Problem_1059 {
+    public static void main(String[] args) {
+        for (int i = 0; i <102; i += 2){
+            System.out.println(i);
+        }
+        sc.close();
+    }
+}
