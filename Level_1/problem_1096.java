fa4b0a7dacfb8f82ddd630ed84c175dab9e8e30a
Matheus Ricardo <matheusri1@hotmail.com>
2024-06-18 22:45:38 -0300

    Create problem_1096.java

---
+++ Level_1/problem_1096.java
@@ -0,0 +1,9 @@
+public class Problem_1096 {
+    public static void main(String[] args) {
+       for (int i = 1; i < 10; i+=2 ){
+            for(int j = 7; 4 < j; j-=1){
+                System.out.printf("I=%d J=%d\n",i,j);
+            }
+        }
+    }
+}
