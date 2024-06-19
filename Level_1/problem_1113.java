aa339dece3bf1f5a1ca4dd5aaf775bfc5057ecb8
Matheus Ricardo <matheusri1@hotmail.com>
2024-06-19 12:53:41 -0300

    Update problem_1113.java

--- Level_1/problem_1113.java
+++ Level_1/problem_1113.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Problem_1003 {
+public class Problem_1113 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<String> resultado = new ArrayList<>();
