86879f18513963744c15f67b2394e3d8a02b3193
Matheus Ricardo <matheusri1@hotmail.com>
2024-06-20 11:12:01 -0300

    Create problem_1144.java

---
+++ Level_1/problem_1144.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Problem_1144 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i + " " + (i * i) + " " + (i * i * i));
+            System.out.println(i + " " + (i * i + 1) + " " + (i * i * i+ 1));
+        }
+    }
+}
